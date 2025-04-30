94235d3ffe9acfca7c1c61e63cfbb98d565dea85
Giulia <giuliamferreira01@gmail.com>
2025-04-30 15:04:34 -0300

    Implementa validação de números romanos com regex e conversão arábica
    - Adiciona expressão regular para validar números romanos válidos até 3000
    - Rejeita padrões inválidos como XXXX, VV, VX, etc. retornando -1
    - Implementa lógica de conversão para inteiros usando mapa de valores
    - Todos os testes atuais passam

--- romano.cpp
+++ romano.cpp
@@ -1,7 +1,18 @@
 #include "romano.hpp"
 #include <map>
+#include <string>
+#include <regex>
 
 int romanoParaArabico(const std::string &romano) {
+    // Expressão regular para validar números romanos até 3000
+    std::regex regex_romano_valido("^M{0,3}(CM|CD|D?C{0,3})"
+                                   "(XC|XL|L?X{0,3})"
+                                   "(IX|IV|V?I{0,3})$");
+
+    if (!std::regex_match(romano, regex_romano_valido)) {
+        return -1;
+    }
+
     std::map<char, int> valores = {
         {'I', 1}, {'V', 5}, {'X', 10},
         {'L', 50}, {'C', 100},
@@ -13,12 +24,11 @@ int romanoParaArabico(const std::string &romano) {
 
     for (int i = romano.size() - 1; i >= 0; --i) {
         char letra = romano[i];
-        if (valores.find(letra) == valores.end()) return -1;
-
         int valor = valores[letra];
-        if (valor < anterior) {
+
+        if (valor < anterior)
             total -= valor;
-        } else {
+        else {
             total += valor;
             anterior = valor;
         }
