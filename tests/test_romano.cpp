03f603a2beec7aebd3e725df1004079eb8545554
Giulia <giuliamferreira01@gmail.com>
2025-04-30 15:09:03 -0300

    Adiciona testes para casos válidos e inválidos de números romanos: passa em todos

--- tests/test_romano.cpp
+++ tests/test_romano.cpp
@@ -8,3 +8,24 @@ TEST(RomanoTest, TesteNumeroValido) {
 TEST(RomanoTest, TesteNumeroInvalido) {
     EXPECT_EQ(romanoParaArabico("XXXX"), -1);
 }
+
+TEST(RomanoTest, TestesValidosComuns) {
+    EXPECT_EQ(romanoParaArabico("IV"), 4);
+    EXPECT_EQ(romanoParaArabico("IX"), 9);
+    EXPECT_EQ(romanoParaArabico("XL"), 40);
+    EXPECT_EQ(romanoParaArabico("XC"), 90);
+    EXPECT_EQ(romanoParaArabico("CD"), 400);
+    EXPECT_EQ(romanoParaArabico("CM"), 900);
+    EXPECT_EQ(romanoParaArabico("MCMXCIV"), 1994);
+    EXPECT_EQ(romanoParaArabico("MMXXV"), 2025);
+}
+
+TEST(RomanoTest, TestesInvalidos) {
+    EXPECT_EQ(romanoParaArabico("IIII"), -1);
+    EXPECT_EQ(romanoParaArabico("VV"), -1);
+    EXPECT_EQ(romanoParaArabico("IC"), -1);
+    EXPECT_EQ(romanoParaArabico("XM"), -1);
+    EXPECT_EQ(romanoParaArabico("IM"), -1);
+    EXPECT_EQ(romanoParaArabico("MMMM"), -1); // maior que 3000
+    EXPECT_EQ(romanoParaArabico("ABCD"), -1); // letras inválidas
+}
